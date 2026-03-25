c2f800884b62ef40d12c2fa248b6186a4cca29b7
Lukas Kubanek <lukas.kubanek@me.com>
2026-03-25 10:39:41 +0100

    pr2-fix-conflict-server-newer

--- Sources/SQLiteData/CloudKit/SyncEngine.swift
+++ Sources/SQLiteData/CloudKit/SyncEngine.swift
@@ -1956,15 +1956,16 @@
         serverRecord.userModificationTime = metadata.userModificationTime
 
         func open<T>(_ table: some SynchronizableTable<T>) throws {
-          var columnNames: [String] = T.TableColumns.writableColumns.map(\.name)
+          var columnNamesToUpsert = Set(T.TableColumns.writableColumns.map(\.name))
           if !force,
             let allFields = metadata._lastKnownServerRecordAllFields,
             let row = try T.unscoped.find(#sql("\(bind: metadata.recordPrimaryKey)")).fetchOne(db)
           {
             serverRecord.update(
               with: allFields,
-              row: T(queryOutput: row),
-              columnNames: &columnNames,
+              clientRow: T(queryOutput: row),
+              clientUserModificationTime: metadata.userModificationTime,
+              columnNamesToUpsert: &columnNamesToUpsert,
               parentForeignKey: foreignKeysByTableName[T.tableName]?.count == 1
                 ? foreignKeysByTableName[T.tableName]?.first
                 : nil
@@ -1973,7 +1974,7 @@
 
           do {
             try $_currentZoneID.withValue(serverRecord.recordID.zoneID) {
-              try #sql(upsert(table, record: serverRecord, columnNames: columnNames)).execute(db)
+              try #sql(upsert(table, record: serverRecord, columnNames: columnNamesToUpsert)).execute(db)
             }
             try UnsyncedRecordID.find(serverRecord.recordID).delete().execute(db)
             try SyncMetadata
